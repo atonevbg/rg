f15c520252b73ac43c43c97fa25c51bbbc497b7a
Anton Tonev <atonevbg@gmail.com>
2015-04-02 00:16:32 +0300

    fixed

--- queries.php
+++ queries.php
@@ -4,23 +4,22 @@ include './connection.php';
 $sql1 = "SELECT * FROM product";
 $product_query = mysqli_query($connection, $sql1);
 while($product_row = mysqli_fetch_assoc($product_query)){
-    $products[] = $product_row;
+    $products[$product_row['id']] = $product_row;
 }
 
 $sql2 = "SELECT * FROM category";
 $category_query = mysqli_query($connection, $sql2);
 while($category_row = mysqli_fetch_assoc($category_query)){
-    $categories[] = $category_row;
+	$categories[$category_row['id']] = $category_row;
 }
 
 $sql3 = "SELECT * FROM product2category";
 $relation_query = mysqli_query($connection, $sql3);
 while($relation_row = mysqli_fetch_assoc($relation_query)){
-    $relations[] = $relation_row;
+    $relations[$relation_row['id']] = $relation_row;
 }
 
-foreach($products as $product) {
-    $products[$product['id']] = $product;
+foreach($products as $p_id => $product) {
     foreach ($product as $key => $value) {
         echo '<b>'.$key.'</b>: '. $value;
         echo '<br>';
@@ -29,24 +28,21 @@ foreach($products as $product) {
 }
 
 echo '<hr>';
-
-foreach($categories as $category) {
-    $categories[$category['id']] = $category;
+foreach($categories as $c_id => $category) {
     foreach ($category as $key => $value) {
         echo '<b>'.$key.'</b>: '. $value;
         echo '<br>';
     }
     echo '<br>';
 }
-
 echo '<hr>';
 
-foreach($relations as $relation) {
-    $relations[$relation['id']] = $relation;
+foreach($relations as $r_id => $relation) {
     foreach ($relation as $key => $value) {
         echo '<b>'.$key.'</b>: '. $value;
         echo '<br>';
     }
     echo '<br>';
+    
 }
 
